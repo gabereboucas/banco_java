e1612313221a00d6169ed730ed1ff40e50da0516
unknown <gabrielrbsr@gmail.com>
2022-06-26 20:45:32 -0300

    a

---
+++ Conta/Conta.js
@@ -0,0 +1,51 @@
+export class Conta{
+    constructor( saldoInicial, cliente, agencia){
+        if(this.constructor == Conta){
+            throw new Error("Você não deve criar essa instacia");
+        }
+
+        this._saldo = saldoInicial;
+        this._cliente = cliente;
+        this._agencia = agencia;
+        
+    }
+
+    set cliente(novoValor){
+        if(novoValor instanceof Cliente){
+        this._cliente = novoValor;
+    }
+}
+
+    get cliente(){
+        return this._cliente;
+    }
+
+    get saldo(){
+        return this._saldo;
+    }
+
+    //metodo abstrato
+    sacar(valor){      
+        throw new Error("O método sacar da conta é abstrato");
+    }
+
+    _sacar(valor,taxa){
+        const valorSacado = taxa * valor;
+        if(this._saldo >= valorSacado){
+            this._saldo -= valorSacado;
+            return valorSacado;  
+        }
+
+        return 0;
+    }
+    
+    depositar(valor){
+        this._saldo += valor;
+    }
+
+    transferir(valor,conta){
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
+        
+    }
+}
